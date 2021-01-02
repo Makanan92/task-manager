6dec97b7ef376f69e839c9504f27f679571027dc
Mohammed <mohaghmir@gmail.com>
2021-01-02 01:37:36 +0100

    Added some python basic examples

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,26 @@
 
 
-print("Hello World Python!")
+def sum_three_parameters(a,b,c):
+     return a+b+c
+
+
+def concatenar(nombre_de_mama):
+    return "Mi madre Guapa se llama " + nombre_de_mama
+
+   
+   
+print(concatenar("Fattouch"))
+
+print(concatenar("Aziza"))
+print(concatenar("paca"))
+
+print(concatenar("juana"))
+
+print(concatenar("manola"))
+
+
+
+
+ 
+ 
+print(sum_three_parameters("Pablo", "pepe", "Juan"))
